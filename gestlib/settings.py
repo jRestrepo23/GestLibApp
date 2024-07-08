d87a3b26cb28f2d23ba2613756037878331a32aa
jrestrepo <you@example.com>
2024-07-08 08:50:31 -0500

    delete contrib

--- gestlib/settings.py
+++ gestlib/settings.py
@@ -134,6 +134,7 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -144,6 +145,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'inicio'
 LOGOUT_REDIRECT_URL = 'inicio'
 
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.staticfiles.storage.CompressedStaticFilesStorage"
-
-COMPRESS_ENABLED = True
